0c5ca1438896f81706d3f67291c6843690f43652
CJ Naujokas <cnaujoka@charlotte.edu>
2024-09-25 14:22:43 -0400

    feat: added function process order to SandwichMaker and updated main()

--- main.py
+++ main.py
@@ -13,6 +13,29 @@ cashier = Cashier()
 
 
 def main():
+    print("Welcome to the Sandwich Maker!")
+    running = True
+    options = ["1: Small sandwich", "2: Medium sandwich", "3: Large sandwich", "4: Off", "5: Report"]
+    while running:
+        x = input(f"{options[0]}\n{options[1]}\n{options[2]}\n{options[3]}\n{options[4]}\nChoose an option: ")
+
+        if x == "1":
+            order = "small"
+            sandwich_maker.process_order(order, recipes, cashier)
+        elif x == "2":
+            order = "medium"
+            sandwich_maker.process_order(order, recipes, cashier)
+        elif x == "3":
+            order = "large"
+            sandwich_maker.process_order(order, recipes, cashier)
+        elif x == "4":
+            print("Bye!")
+            running = False
+        elif x == "5":
+            for item in sandwich_maker.machine_resources:
+                print(f"{item}: {sandwich_maker.machine_resources[item]}")
+        else:
+            print("please pick a valid option")
 
 
 
--- sandwich_maker.py
+++ sandwich_maker.py
@@ -14,3 +14,10 @@ class SandwichMaker:
     def make_sandwich(self, sandwich_size, order_ingredients):
         for item in order_ingredients:
             self.machine_resources[item] -= order_ingredients[item]
+
+    def process_order(self, order, recipe, cashier):
+        if self.check_resources(recipe[order]["ingredients"]):
+            y = cashier.process_coins()
+            if cashier.transaction_result(y, recipe[order]["cost"]):
+                self.make_sandwich(recipe[order], recipe[order]["ingredients"])
+                print(f"{order} sandwich is ready!")
